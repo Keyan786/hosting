59a8f3af1bd0651b47fab31cc23dd9ce620cc433
Keyan786 <keyangec@gmail.com>
2025-04-02 12:43:03 +0530

    Initial commit

---
+++ run.ps1
@@ -0,0 +1,3 @@
+cd web-inter-prep-main
+py -3.11 -m pip install "flask==2.0.1" "flask-sqlalchemy==2.5.1" "flask-wtf==0.15.1" "werkzeug==2.0.1" "sqlalchemy==1.4.23" "bcrypt==4.0.1" "email_validator==1.1.3"
+py -3.11 app.py 
---
+++ start.ps1
@@ -0,0 +1,2 @@
+py -3.11 -m pip install "flask==2.0.1" "flask-sqlalchemy==2.5.1" "flask-wtf==0.15.1" "werkzeug==2.0.1" "sqlalchemy==1.4.23" "bcrypt==4.0.1"
+py -3.11 app.py 
